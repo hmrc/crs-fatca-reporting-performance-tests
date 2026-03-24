0ef636b4350a736a9ce57bbc4fd85ca2f07d6fa1
vigod-nc <vinay.godena@netcompany.com>
2026-03-24 16:48:14 +0000

    test-runner-update

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   private val gatlingVersion = "3.4.2"
 
   val test = Seq(
-    "uk.gov.hmrc"          %% "performance-test-runner"   % "6.2.0"         % Test,
+    "uk.gov.hmrc"          %% "performance-test-runner"   % "6.3.0"         % Test,
   )
 
 }
